41886a0d489cb67d4bca6593c023eb66d8ca4482
dasoncheng <dasoncheng@outlook.com>
2022-05-07 19:44:40 +0800

    feat: demo update

--- app/src/views/development.tsx
+++ app/src/views/development.tsx
@@ -1,9 +1,11 @@
+import { css } from "@emotion/css";
 import { defineComponent, reactive } from "vue";
 import {
   Alert,
   Breadcrumb,
   BreadcrumbItem,
   Button,
+  Card,
   Carousel,
   Col,
   Dialog,
@@ -43,216 +45,223 @@ export const Development = defineComponent({
       name: "",
     });
     return () => (
-      <>
+      <div
+        class={css({
+          h3: {
+            margin: "20px 0 10px",
+          },
+          ".l-card": {
+            width: "100%",
+            marginBottom: "16px",
+          },
+        })}
+      >
         <Row gutter={[16, 16]}>
           <Col span={24}>
-            <Tabs>
-              <TabPane name="apple" tab="苹果">
-                乔布斯
-              </TabPane>
-              <TabPane name="microsoft" tab="微软">
-                比尔盖茨
-              </TabPane>
-              <TabPane name="google" tab="谷歌">
-                拉里佩奇
-              </TabPane>
-            </Tabs>
-          </Col>
-          <Col span={24}>
-            <Space>
-              <Button theme="primary">Slot String</Button>
-              <Button theme="primary">
-                <span>Slot Span</span>
-              </Button>
-              {/* <Button
-              theme="primary"
-              v-slots={{
-                default: "VSlot String",
-              }}
-            /> */}
-              {/* <Button
-              theme="primary"
-              v-slots={{
-                default: <span>VSlot Span</span>,
-              }}
-            /> */}
-              <Button
-                theme="primary"
+            <h3>### 通用</h3>
+
+            <Card title="按钮 Button">
+              <Space>
+                <Button theme="primary">Slot String</Button>
+                <Button theme="primary">
+                  <span>Slot Span</span>
+                </Button>
+                <Button
+                  theme="primary"
+                  v-slots={{
+                    default: () => <span>VSlot Function</span>,
+                  }}
+                />
+                <Button theme="primary" content="Prop String" />
+                <Button theme="primary" content={<span>Prop Span</span>} />
+                <Button theme="primary" content={() => <span>Prop Function</span>} />
+              </Space>
+            </Card>
+            <Card title="分割线 Divider">
+              <p>
+                这个
+                <Divider direction="vertical" />
+                看起来
+                <Divider direction="vertical" />
+                好像不是很显眼
+              </p>
+            </Card>
+            <Card title="下拉菜单 Dropdown">
+              <Dropdown options={<span>item 1</span>}>
+                <Button theme="primary">Hover me</Button>
+              </Dropdown>
+            </Card>
+            <Card title="图标 Icon">
+              <Icon size={16} color="yellow" name="home" />
+            </Card>
+
+            <h3>### 输入</h3>
+
+            <Card title="级联选择器 Cascader"></Card>
+            <Card title="复选框 Checkbox"></Card>
+            <Card title="日期选择器 DatePicker"></Card>
+            <Card title="表单 Form">
+              <Form>
+                <FormItem
+                  rules={[
+                    { message: "必填项", type: "required" },
+                    { message: "数值不小于0", type: "min", value: 0 },
+                    { message: "数值不大于200", type: "max", value: 200 },
+                  ]}
+                ></FormItem>
+                <FormItem
+                  rules={[
+                    Validators.required(),
+                    Validators.minLength(0),
+                    Validators.maxLength(200),
+                    Validators.pattern(/^(13[0-9]|15[0|1|3|6|7|8|9]|18[8|9])\d{8}$/),
+                  ]}
+                ></FormItem>
+              </Form>
+            </Card>
+            <Card title="输入框 Input">
+              <Input v-model:value={data.name} />
+            </Card>
+            <Card title="数字输入框 InputNumber"></Card>
+            <Card title="单选框 Radio"></Card>
+            <Card title="选择器 Select"></Card>
+            <Card title="开关 Switch"></Card>
+            <Card title="时间选择器 TimePicker"></Card>
+            <Card title="上传 Upload"></Card>
+
+            <h3>### 布局</h3>
+
+            <Card title="栅格 Grid"></Card>
+            <Card title="间距 Space"></Card>
+
+            <h3>### 展示</h3>
+
+            <Card title="轮播图 Carousel">
+              {" "}
+              <Carousel>
+                <img
+                  class="carousel-img"
+                  src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+                />
+                <img
+                  class="carousel-img"
+                  src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+                />
+                <img
+                  class="carousel-img"
+                  src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+                />
+                <img
+                  class="carousel-img"
+                  src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+                />
+              </Carousel>
+            </Card>
+            <Card title="骨架屏 Skeleton">
+              <Skeleton repeat={3} />
+            </Card>
+            <Card title="表格 Table">
+              <Table />
+            </Card>
+            <Card title="标签 Tag">
+              <Space>
+                <Tag>标签一</Tag>
+                <Tag theme="error">标签一</Tag>
+                <Tag theme="primary">标签一</Tag>
+                <Tag theme="success">标签一</Tag>
+                <Tag theme="warning">标签一</Tag>
+              </Space>
+            </Card>
+
+            <h3>### 反馈</h3>
+
+            <Card title="警告 Alert">
+              <Alert theme="success" message="这是一条成功的消息提示" />
+            </Card>
+            <Card title="对话框 Dialog">
+              <Dialog title="确认" content="你确定" />
+            </Card>
+            <Card title="全局提示 Message">
+              <Message theme="success">用于表示操作顺利达成</Message>
+            </Card>
+            <Card title="气泡确认框 Popconfirm">
+              <Popconfirm title="Are you sure delete this task?">
+                <a href="#">Delete</a>
+              </Popconfirm>
+            </Card>
+            <Card title="气泡卡片 Popover">
+              <Popover
+                title="Title"
                 v-slots={{
-                  default: () => <span>VSlot Function</span>,
+                  content: () => (
+                    <>
+                      <p>Content</p>
+                      <p>Content</p>
+                    </>
+                  ),
                 }}
-              />
-            </Space>
-          </Col>
-          <Col span={24}>
-            <Alert>这是一条成功的消息提示</Alert>
-          </Col>
-          <Col span={24}>
-            <Space>
-              <Tag>标签一</Tag>
-              <Tag theme="error">标签一</Tag>
-              <Tag theme="primary">标签一</Tag>
-              <Tag theme="success">标签一</Tag>
-              <Tag theme="warning">标签一</Tag>
-            </Space>
-          </Col>
-          <Col span={24}>
-            <Space>
-              <Button theme="primary" content="Prop String" />
-              <Button theme="primary" content={<span>Prop Span</span>} />
-              <Button theme="primary" content={() => <span>Prop Function</span>} />
-            </Space>
-          </Col>
-          <Col span={24}>
-            <Skeleton repeat={3} />
-          </Col>
-          <Col span={24}>
-            <Breadcrumb>
-              <BreadcrumbItem>湖北</BreadcrumbItem>
-              <BreadcrumbItem>武汉</BreadcrumbItem>
-              <BreadcrumbItem>洪山</BreadcrumbItem>
-            </Breadcrumb>
-          </Col>
-          <Col>
-            <Input v-model:value={data.name} />
-            <input v-model={data.name} />
-            <p>{data.name}</p>
+              >
+                <Button theme="primary">Hover me</Button>
+              </Popover>
+            </Card>
+            <Card title="文字提示 Tooltip">
+              <Tooltip title="prompt text">Tooltip will show when mouse enter.</Tooltip>
+            </Card>
+
+            <h3>### 导航</h3>
+
+            <Card title="面包屑 Breadcrumb">
+              <Breadcrumb>
+                <BreadcrumbItem>湖北</BreadcrumbItem>
+                <BreadcrumbItem>武汉</BreadcrumbItem>
+                <BreadcrumbItem>洪山</BreadcrumbItem>
+              </Breadcrumb>
+            </Card>
+            <Card title="导航菜单 Menu">
+              <Menu>
+                <MenuItem>菜单项</MenuItem>
+                <MenuItem>菜单项</MenuItem>
+                <MenuItem>菜单项</MenuItem>
+                <MenuSub title="子菜单">
+                  <MenuItem>子菜单项</MenuItem>
+                  <MenuItem>子菜单项</MenuItem>
+                  <MenuItem>子菜单项</MenuItem>
+                </MenuSub>
+              </Menu>
+            </Card>
+            <Card title="分页 Pagination">
+              <Pagination total={100} page={1} size={20} options={[10, 20, 30, 50, 100]} />
+            </Card>
+            <Card title="步骤条 Steps">
+              <Steps>
+                <StepItem title="I Me Mine" description="All through the day, I me mine I me mine, I me mine" />
+                <StepItem
+                  title="Let It Be"
+                  description="When I find myself in times of trouble Mother Mary comes to me"
+                />
+                <StepItem title="Come Together" description="Here come old flat top He come grooving up slowly" />
+                <StepItem
+                  title="Something"
+                  description="Something in the way she moves Attracts me like no other lover"
+                />
+              </Steps>
+            </Card>
+            <Card title="选项卡 Tabs">
+              <Tabs>
+                <TabPane name="apple" tab="苹果">
+                  乔布斯
+                </TabPane>
+                <TabPane name="microsoft" tab="微软">
+                  比尔盖茨
+                </TabPane>
+                <TabPane name="google" tab="谷歌">
+                  拉里佩奇
+                </TabPane>
+              </Tabs>
+            </Card>
           </Col>
         </Row>
-        <h3>General</h3>
-        <Button>Action</Button>
-        <p>
-          这个
-          <Divider direction="vertical" />
-          看起来
-          <Divider direction="vertical" />
-          好像不是很显眼
-        </p>
-        <Dropdown
-          v-slots={{
-            overlay: () => (
-              <>
-                <p>item one</p>
-              </>
-            ),
-          }}
-        >
-          <a>Hover me</a>
-        </Dropdown>
-        <Icon size={16} color="yellow" name="home" />
-        <h3>Entry</h3>
-        <Form>
-          <FormItem
-            rules={[
-              { message: "必填项", type: "required" },
-              { message: "数值不小于0", type: "min", value: 0 },
-              { message: "数值不大于200", type: "max", value: 200 },
-            ]}
-          ></FormItem>
-          <FormItem
-            rules={[
-              Validators.required(),
-              Validators.minLength(0),
-              Validators.maxLength(200),
-              Validators.pattern(/^(13[0-9]|15[0|1|3|6|7|8|9]|18[8|9])\d{8}$/),
-            ]}
-          ></FormItem>
-        </Form>
-        <h3>Layout</h3>
-        <Row>
-          <Col>8</Col>
-          <Col>8</Col>
-          <Col>8</Col>
-        </Row>
-        <Space>
-          <Button>Action</Button>
-          <Button>Action</Button>
-        </Space>
-        <h3>Display</h3>
-        <Carousel>
-          <img class="carousel-img" src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg" />
-          <img class="carousel-img" src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg" />
-          <img class="carousel-img" src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg" />
-          <img class="carousel-img" src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg" />
-        </Carousel>
-        <Skeleton text />
-        <Table />
-        <Tag theme="primary">标签一</Tag>
-        <h3>Feedback</h3>
-        <Alert theme="success" message="这是一条成功的消息提示" />
-        <Dialog title="确认" content="你确定" />
-        <Button
-          onClick={() => {
-            dialog.create({
-              content: "你确定",
-              title: "确认",
-            });
-          }}
-        >
-          Confirm
-        </Button>
-        <Message theme="success">用于表示操作顺利达成</Message>
-        <Button
-          onClick={() => {
-            message.create({
-              content: "Are you sure delete this task?",
-            });
-          }}
-        >
-          Confirm
-        </Button>
-        <Popconfirm title="Are you sure delete this task?">
-          <a href="#">Delete</a>
-        </Popconfirm>
-        <Popover
-          title="Title"
-          v-slots={{
-            content: () => (
-              <>
-                <p>Content</p>
-                <p>Content</p>
-              </>
-            ),
-          }}
-        >
-          <Button theme="primary">Hover me</Button>
-        </Popover>
-        <Tooltip title="prompt text">Tooltip will show when mouse enter.</Tooltip>
-        <h3>Navigation</h3>
-        <Breadcrumb>
-          <BreadcrumbItem>湖北</BreadcrumbItem>
-          <BreadcrumbItem>武汉</BreadcrumbItem>
-          <BreadcrumbItem>洪山</BreadcrumbItem>
-        </Breadcrumb>
-        <Menu>
-          <MenuItem>菜单项</MenuItem>
-          <MenuItem>菜单项</MenuItem>
-          <MenuItem>菜单项</MenuItem>
-          <MenuSub title="子菜单">
-            <MenuItem>子菜单项</MenuItem>
-            <MenuItem>子菜单项</MenuItem>
-            <MenuItem>子菜单项</MenuItem>
-          </MenuSub>
-        </Menu>
-        <Pagination total={100} page={1} size={20} options={[10, 20, 30, 50, 100]} />
-        <Steps>
-          <StepItem title="I Me Mine" description="All through the day, I me mine I me mine, I me mine" />
-          <StepItem title="Let It Be" description="When I find myself in times of trouble Mother Mary comes to me" />
-          <StepItem title="Come Together" description="Here come old flat top He come grooving up slowly" />
-          <StepItem title="Something" description="Something in the way she moves Attracts me like no other lover" />
-        </Steps>
-        <Tabs>
-          <TabPane name="oasis" tab="Oasis">
-            Wonderwall
-          </TabPane>
-          <TabPane name="the beatles" tab="the Beatles">
-            Hey Jude
-          </TabPane>
-          <TabPane name="jay chou" tab="周杰伦">
-            七里香
-          </TabPane>
-        </Tabs>
-      </>
+      </div>
     );
   },
 });
